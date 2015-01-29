217f4badf21e1e4fdf15bbe2092950cdaed965af
Szikszai Gusztáv <guszti5@hotmail.com>
2015-01-29 16:33:02 +0100

    Only set the endpoint if the src starts with http.

--- opal/fron/active_record.rb
+++ opal/fron/active_record.rb
@@ -4,4 +4,5 @@ require 'fron/active_record/manager'
 require 'fron/active_record/record'
 require 'fron/active_record/models'
 
-Fron::ActiveRecord::Manager.endpoint = DOM::Element.new(`Array.prototype.slice.call(document.getElementsByTagName("script"))`.last)[:src].split('/')[0..2].join('/')
+src = DOM::Element.new(`Array.prototype.slice.call(document.getElementsByTagName("script"))`.last)[:src]
+Fron::ActiveRecord::Manager.endpoint = src.split('/')[0..2].join('/') if src =~ /^http/
